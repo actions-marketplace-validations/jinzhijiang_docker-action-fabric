5739317b97ed9760f17145db4cb7d0cb2a03d1d1
Xu Jiaji <jiajixu@qq.com>
2022-04-26 18:39:20 +0800

    feat: 增加ssh密码配置

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,11 +2,16 @@
 
 cd $GITHUB_WORKSPACE
 mv /action_fabfile_creator.py .
-echo "$5"
-mkdir ~/.ssh
-chmod 700 ~/.ssh
-echo "$1" > ~/.ssh/id_rsa
-chmod 600 ~/.ssh/id_rsa
-python3 action_fabfile_creator.py -u $2 -i $3 -P $4 -k ~/.ssh/id_rsa -c "$5"
+if [ ! $1 ]; then
+	echo "配置的SSH RSA"
+	mkdir ~/.ssh
+	chmod 700 ~/.ssh
+	echo "$1" > ~/.ssh/id_rsa
+	chmod 600 ~/.ssh/id_rsa
+	python3 action_fabfile_creator.py -u $2 -i $3 -P $4 -k ~/.ssh/id_rsa -c "$5"
+else
+	echo "配置的SSH密码"
+	python3 action_fabfile_creator.py -u $2 -i $3 -P $4 -p $6 -c "$5"
+fi
 cat action_fabfile.py
 fab -f action_fabfile.py deploy
