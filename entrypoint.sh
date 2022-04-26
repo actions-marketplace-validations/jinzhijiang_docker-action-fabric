5f79e8ed2c02ff4c48922f9162ec1d1be989d399
Xu Jiaji <jiajixu@qq.com>
2022-04-26 14:32:50 +0800

    feat: 创建ssh rsa

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,10 @@
 cd $GITHUB_WORKSPACE
 mv /action_fabfile_creator.py .
 echo "$5"
-python3 action_fabfile_creator.py -u $2 -i $3 -P $4 -k ~/ssh/.id_rsa -c "$5"
+mkdir ~/.ssh
+chmod 700 ~/.ssh
+echo "$1" > ~/.ssh/id_rsa
+chmod 600 ~/.ssh/id_rsa
+python3 action_fabfile_creator.py -u $2 -i $3 -P $4 -k ~/.ssh/id_rsa -c "$5"
 cat action_fabfile.py
 fab -f action_fabfile.py deploy
