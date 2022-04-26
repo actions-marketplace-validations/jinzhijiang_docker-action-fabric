be2fe067c51479d87a9bef327472005f7ad3c41e
Xu Jiaji <jiajixu@qq.com>
2022-04-26 11:25:34 +0800

    feat: 调整代码

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,8 +2,8 @@
 
 cd $GITHUB_WORKSPACE
 mv /action_fabfile_creator.py .
+python3 action_fabfile_creator.py -u $2 -i $3 -P $4 -k ~/ssh/.id_rsa -c $5
 pwd
 ls
-python3 action_fabfile_creator.py -u $2 -i $3 -P $4 -k ~/ssh/.id_rsa -c $5
 cat fabfile_creator.py
 fab -f fabfile_creator.py deploy
