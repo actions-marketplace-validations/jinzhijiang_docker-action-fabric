ffc34bedf805eca4727b6a6ca83178beb0277b96
Xu Jiaji <jiajixu@qq.com>
2022-04-26 11:46:05 +0800

    feat: 调整py代码

--- action_fabfile_creator.py
+++ action_fabfile_creator.py
@@ -44,13 +44,7 @@ with open('action_fabfile.py', 'w') as af:
 
 	pcode = ('env.password = "%s"' % password) if key_filepath is None else ("env.key_filename = '%s'" % key_filepath)
 
-    s = """
-            local('pwd')
-            local('ls')
-    """
-    print(reduce(lambda a, b: a.strip() + "\n\t" + b.strip(), s.strip().split("\n")))
-
-    pycode = reduce(lambda a, b: a.strip() + "\n\t" + b.strip(), code.strip().split("\n"))
+    pycode = reduce(lambda a, b: "%s\n\t%s" % (a.strip(), b.strip()), code.strip().split("\n"))
 
 	af.write('''#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
